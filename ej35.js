07292bfebb6aee1b19c81765c3c42c6e8ef44ad7
Alicia Maiuri <aicilam@gmail.com>
2019-10-26 21:48:58 -0300

    Primer commit ejercici35

---
+++ ej35.js
@@ -0,0 +1,4 @@
+numeroDeCliente  = 143245
+numeroDeClienteIngresado = '143245'
+console.log ("El numero ingresado por el cliente es correcto ", (numeroDeCliente == numeroDeClienteIngresado))
+console.log ("El cliente ingresó un número de cliente válido ", (numeroDeCliente === numeroDeClienteIngresado))
